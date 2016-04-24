85c9825a6abb41febad4ab12022b1a5e6f7d736a
Tenocci <sekelj13@highpoint.edu>
2016-04-24 11:36:50 -0400

    swag

--- main.js
+++ main.js
@@ -17,6 +17,18 @@ var secondaryMeaning = 'Heir';
 var baseline = 0.74;
 var selectionThreshold = 0.6;
 
+//numNodes:int - number of nodes in each array
+//ratio: double - arr1/arr2
+function randomInit(numNodes,ratio,arr1,arr2)
+{
+  for(i=0;i < numNodes;i++) {
+    arr1[i].weight=Math.random();
+    if(arr1[i].weight/ratio>1)
+      arr2[i].weight=arr1[i].weight*ratio;
+    else arr2[i].weight=arr1[i].weight/ratio;
+  }
+}
+
 function generateArray(count) {
 
    dominantAttributeWeights = [];
@@ -88,3 +100,5 @@ window.onload = function () {
    generateArray(attributeCount);
    reloadAttributes();
 };
+
+
